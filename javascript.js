c891a9080da73d3845fee438134c71b4eb416ca4
Alejandro Varela <a23alejandrovp@iessanclemente.net>
2025-01-20 17:33:12 +0100

    first version

---
+++ javascript.js
@@ -0,0 +1,10 @@
+function showImage(){
+    let happyImage = document.getElementById("happyImg");
+    let tenderImage = document.getElementById("tenderImg");
+    image.classList.remove("opacity-0");
+}
+
+function hideImage(){
+    let happyImage = document.getElementById("happyImg");
+    image.classList.add("opacity-0");
+}
